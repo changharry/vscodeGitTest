0d4fd61df21c41c33a7b46959f0bd2cfc505f451
changharry <harrychangzh@gmail.com>
2020-01-22 10:35:59 -0800

    second commit

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
+//init modules
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+//router
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+
 
 var app = express();
 
