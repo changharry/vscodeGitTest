50a01d1f46403115d93dc01fd467ce7b450a6dc8
changharry <harrychangzh@gmail.com>
2020-01-22 10:38:28 -0800

    third test

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-//router
+//router setup
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
